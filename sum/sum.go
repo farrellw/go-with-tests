9d3e2d9a8bb54a96a548020d525db38e37ea4f89
farrellw <farrell.will.g@gmail.com>
2018-05-14 11:41:42 -0500

    Add sum tests.

--- sum/sum.go
+++ sum/sum.go
@@ -8,3 +8,28 @@ func Sum(numbers []int) (sum int) {
 	
 	return
 }
+
+func SumAll(numbersToSum ...[]int) (sums []int){
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return
+}
+
+func SumAllTails(numbersToSum ...[]int) (sums []int){
+	
+	for _, numbers := range numbersToSum {
+		if len(numbers) != 0 {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		} else {
+			sums = append(sums, 0)
+		}
+
+	}
+
+	return
+}
+
--- sum/sum_test.go
+++ sum/sum_test.go
@@ -1,13 +1,54 @@
 package sum
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
-func TestSum(t *testing.T){
-	numbers := []int{10, 4, 7, 9}
-	actual := Sum(numbers)
-	expected := 30
-
+func assertCorrectMessage(t *testing.T, expected int, actual int, params []int) {
+	t.Helper()
 	if actual != expected {
-		t.Errorf("Expected '%d', but got '%d', inputs: '%v'", expected, actual, numbers)
+		t.Errorf("Expected '%d', but got '%d', inputs: '%v'", expected, actual, params)
 	}
+}
+
+func TestSum(t *testing.T) {
+	t.Run("Collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+		actual := Sum(numbers)
+		expected := 6
+
+		assertCorrectMessage(t, expected, actual, numbers)
+	})
+}
+
+func assertDeepEqual(t *testing.T, expected []int, actual []int) {
+	t.Helper()
+
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v, wanted %v", actual, expected)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	assertDeepEqual(t, want, got)
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("Make the sums of slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 2, 9})
+		want := []int{2, 11}
+
+		assertDeepEqual(t, want, got)
+	})
+
+	t.Run("Saveful sum empty slices", func(t *testing.T){
+		got := SumAllTails([]int{}, []int{2,9})
+		want := []int{0, 9}
+
+		assertDeepEqual(t, want, got)
+	})
 }
